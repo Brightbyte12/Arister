6c7b9c30b8f80973561894654837d660461e8796
Brightbyte12 <brightbyte.team@gmail.com>
2025-07-04 12:15:37 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -25,7 +25,7 @@ const forgotPasswordRoutes = require('./routes/forgotPassword');
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({
